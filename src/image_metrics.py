247375fe4eb3375f61dc457064844d7f5c327446
Iryna Syzova <ira.sizova@gmail.com>
2020-08-19 00:00:29 +0000

    big selection update

---
+++ src/image_metrics.py
@@ -0,0 +1,68 @@
+"""
+Self-defined functions to measure images according to different properties.
+"""
+import cv2
+import numpy as np
+from skimage.metrics import structural_similarity as ssim, normalized_root_mse
+
+from src.Tile import Tile
+
+
+def contrast_measure(img):
+    return np.median([img[:, :, i].max() - img[:, :, i].min() for i in [0, 1, 2]])/255.0
+
+
+def get_tile_contrast(tile, n_pieces=25):
+    tile_list = tile.get_pieces(n_pieces)
+    return min([contrast_measure(_.img) for _ in tile_list])
+
+
+def _tile_symmetry_helper(tile, metric='ssim'):
+    if metric not in ('ssim', 'normalized_root_mse'):
+        print('{} is not currently supported'.format(metric))
+        return
+    
+    tile_compare = [
+        [tile, tile.flip_horizontal()],
+        [tile, tile.flip_vertical()],
+        [tile, tile.flip_transpose()],
+        [tile, tile.rotate(clockwise=False).flip_transpose().rotate(clockwise=True)]
+    ]
+
+    symmetry_measure_list = []
+    for i in range(len(tile_compare)):
+        tile0, tile1 = tile_compare[i]
+        if metric == 'ssim':
+            symmetry_measure = ssim(tile0.img, tile1.img, multichannel=True)
+        elif metric == 'normalized_root_mse':
+            symmetry_measure = normalized_root_mse(tile0.img, tile1.img)
+        symmetry_measure_list.append(symmetry_measure)
+
+    if metric == 'ssim':
+        return np.max(symmetry_measure_list)
+    if metric == 'normalized_root_mse':
+        return np.min(symmetry_measure_list)
+
+    
+def get_tile_symmetry(tile):
+    symmetry_measure = {}
+    
+    symmetry_measure['ssim'] = _tile_symmetry_helper(tile, metric='ssim')
+    symmetry_measure['normalized_root_mse'] = _tile_symmetry_helper(tile, metric='normalized_root_mse')
+    
+    return symmetry_measure
+
+
+def get_tile_symmetry_by_piece(tile, pieces=(4, 9, 16, 25, 36, 49, 64)):
+    symmetry_pieces = {'ssim': [], 'normalized_root_mse': []}
+    for n in pieces:
+        tile_list = tile.get_pieces(n)
+        
+        symmetry_by_piece = np.min([_tile_symmetry_helper(tile_piece, metric='ssim') for tile_piece in tile_list])
+        symmetry_pieces['ssim'].append(symmetry_by_piece)
+            
+        symmetry_by_piece = np.max([_tile_symmetry_helper(tile_piece, metric='normalized_root_mse') for tile_piece in tile_list])
+        symmetry_pieces['normalized_root_mse'].append(symmetry_by_piece)
+        
+    return {'ssim': max(symmetry_pieces['ssim']), 'normalized_root_mse': min(symmetry_pieces['normalized_root_mse']) }
+    
--- src/image_selection.py
+++ src/image_selection.py
@@ -6,6 +6,9 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+from src import image_metrics
+from src.Tile import Tile
+
 
 def get_square_imgs(img_list, folder = '', tolerance=0.1, plot_aspect_ratios=True):
     """
@@ -17,7 +20,6 @@ def get_square_imgs(img_list, folder = '', tolerance=0.1, plot_aspect_ratios=Tru
     :param plot_aspect_ratios: if True, plots aspect ratios
     :return: list of square-ish tiles
     """
-
     aspect_ratios = [0] * len(img_list)
 
     i = 0
@@ -49,3 +51,111 @@ def get_square_imgs(img_list, folder = '', tolerance=0.1, plot_aspect_ratios=Tru
         plt.show()
         
     return sample_square
+
+
+def get_contrast_imgs(img_list, folder = '', min_contrast=0.25, n_pieces=25, plot_contrasts=True):
+    """
+    Returns list of files that are square or almost square
+    
+    :param img_list: list of image names 
+    :param folder: path to images
+    :param min_contrast:minimum possible contast measure as defined in image_metrics.py
+    :param plot_aspect_ratios: if True, plots aspect ratios
+    :return: list of contasting enough files
+    """
+    contrast_measure_list = [0] * len(img_list)
+    sample_contrasting = []
+
+    for i, img_file in enumerate(img_list):
+        img = cv2.imread('{}/{}'.format(folder, img_file))[...,::-1]
+        img = _prepare_img(img)
+
+        contrast_measure_list[i] =  image_metrics.get_tile_contrast(Tile(img), n_pieces=25)
+        if contrast_measure_list[i] > min_contrast:
+            sample_contrasting.append(img_file)
+
+    if plot_contrasts:
+        plt.plot(sorted(contrast_measure_list));
+        plt.gca().axhline(min_contrast, color='red')
+        title = 'Contrast measures of the images'
+        plt.title(title)
+        plt.show()
+    
+    return sample_contrasting
+
+
+def get_symmetric_imgs(img_list, folder = '', cut_offs={'mse': 0.05, 'ssim':0.95}, plot_symmetry_measures=True):
+    """
+    Returns list of files that are square or almost square
+    
+    :param img_list: list of image names 
+    :param folder: path to images
+    :param cut_offs: minimum symmetry measure by metric as defined in image_metrics.py
+    :param plot_symmetry_measures: if True, plots symmetry measures
+    :return: list of symmetric enough files + list of images consisting of multiple symmetric tiles
+    """
+
+    symmetry_measure = [None] * len(img_list)
+    symmetry_pieces = [None] * len(img_list)
+    
+    sample_symmetric = []
+    sample_multitile = []
+    
+    cut_off_mse = cut_offs['mse']
+    cut_off_ssim=cut_offs['ssim']
+    
+    for i, img_file in enumerate(img_list):
+        img = cv2.imread('{}/{}'.format(folder, img_file))[...,::-1]
+    
+        symmetry_measure[i] = image_metrics.get_tile_symmetry(Tile(_prepare_img(img)))
+        symmetry_pieces[i] = image_metrics.get_tile_symmetry_by_piece(Tile(_prepare_img(img, new_size=(512, 512))))
+
+        if (symmetry_measure[i]['ssim'] >= cut_off_ssim) or \
+            (symmetry_measure[i]['normalized_root_mse'] <= cut_off_mse):
+            sample_symmetric.append(img_file)
+        if (symmetry_pieces[i]['ssim'] >= cut_off_ssim) or \
+            (symmetry_pieces[i]['normalized_root_mse'] <= cut_off_mse):
+            sample_multitile.append(img_file)
+
+    if plot_symmetry_measures:
+        fig, ax = plt.subplots(1, 2, figsize=(12, 4))
+        ax[0].plot(sorted([_['normalized_root_mse'] for _ in symmetry_measure]));
+        ax[0].axhline(cut_off_mse, color='red')
+        title = 'Symmetry measures of the images (mse)'
+        ax[0].set_title(title)
+        
+        ax[1].plot(sorted([_['ssim'] for _ in symmetry_measure]));
+        ax[1].axhline(cut_off_ssim, color='red')
+        title = 'Symmetry measures of the images (ssim)'
+        ax[1].set_title(title)
+        plt.show()
+        
+        fig, ax = plt.subplots(1, 2, figsize=(12, 4))
+        ax[0].plot(sorted([_['normalized_root_mse'] for _ in symmetry_pieces]));
+        ax[0].axhline(cut_off_mse, color='red')
+        title = 'Symmetry measures of the images by pieces (mse)'
+        ax[0].set_title(title)
+
+        ax[1].plot(sorted([_['ssim'] for _ in symmetry_pieces]));
+        ax[1].axhline(cut_off_ssim, color='red')
+        title = 'Symmetry measures of the images by pieces (ssim)'
+        ax[1].set_title(title)
+        plt.show()
+    
+    return sample_symmetric, sample_multitile
+
+
+def _prepare_img(img, new_size=(128, 128), blurring_kernel=(3, 3)):
+    """
+    Reshapes and blurs image in preparation for contrast and symmetry checks.
+    :param img: image to prepare
+    :return: prepared image
+    """
+    new_shape = min(img.shape[0], img.shape[1])
+    img = img[0:new_shape, 0:new_shape, ...]
+
+    img = cv2.resize(img, new_size)
+    img = cv2.GaussianBlur(img, blurring_kernel, 0)
+        
+    return img 
+    
