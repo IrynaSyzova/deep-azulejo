c1367a2734ffba9ca86e711f4ab6e137597270f8
IrynaSyzova <ira.sizova@gmail.com>
2020-09-11 22:16:20 +0100

    logging utils

---
+++ src/logging.py
@@ -0,0 +1,20 @@
+import logging
+import sys
+
+def getLogger(name):
+    logger = logging.getLogger(name)
+    # TODO: Move away
+    logger.setLevel(logging.DEBUG)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler(stream=sys.stdout)
+    ch.setLevel(logging.DEBUG)
+
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
