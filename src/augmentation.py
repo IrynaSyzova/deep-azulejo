555f00d99daedc9d9e9da496158bd7a97ebd0484
IrynaSyzova <ira.sizova@gmail.com>
2020-09-11 20:25:11 +0100

    saving results

--- src/augmentation.py
+++ src/augmentation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import uuid
 
 from collections import deque
 from itertools import product, combinations
@@ -7,58 +8,72 @@ from src import Tile
 from src import image_utils
 
 
-def rearrange(tile, min_size, max_size):
+def enrich(tile, save_func, scale_min=4, scale_max=4):
     """
     Creates a list of new tiles obtained by cutting and gluing current tile.
 
     :param tile: starting tile
-    :param min_size: minimum size of obtained tile
-    :param max_size: maximum size of obtained tile
-    :return: list of generated tiles
+    :param save_func: function to write generated images; should be called as save_func(image, image_path)
+    :param scale_min: how small minimum size of obtained tile can be relative to starting tile
+    :param scale_max: how big maximum size of obtained tile can be relative to starting tile
+    :return: None
     """
+
+    min_size = tile.img.dims[0] // scale_min
+    max_size = tile.img.dims[0] // scale_max
+
+    def __save_tile(tile):
+        for x in enrich_colour(tile):
+            for y in enrich_contrast(x):
+                img_name = str(uuid.uuid4())
+                save_func(y.img, '{}.jpg'.format(img_name))
+
     to_fragment = deque()
     to_augment = deque()
-    fragments = deque()
 
     to_fragment.append(tile)
 
     while to_fragment:
         curr = to_fragment.popleft()
 
-        fragments.append(curr)
+        __save_tile(curr)
 
         if curr.get_rhombus().dims[0] >= min_size:
-            fragments.append(curr.get_rhombus())
-            to_fragment.append(curr.get_rhombus())
-            to_augment.append(curr.get_rhombus())
+            rhombus = curr.get_rhombus()
+            to_fragment.append(rhombus)
+            to_augment.append(rhombus)
+            __save_tile(rhombus)
 
-            fragments.append(curr.remove_center())
-            to_fragment.append(curr.remove_center())
-            to_augment.append(curr.remove_center())
+            no_center = curr.remove_center()
+            to_fragment.append(no_center)
+            to_augment.append(no_center)
+            __save_tile(no_center)
 
         if curr.get_quadrant(0, 0).dims[0] >= min_size:
             for i in range(0, 2):
                 for j in range(0, 2):
-                    fragments.append(curr.assemble_quadrant_unfold(i, j))
+                    __save_tile(curr.assemble_quadrant_unfold(i, j))
 
         if curr.dims[0] * 4 <= max_size:
             to_augment.append(curr)
 
     while to_augment:
         curr = to_augment.pop()
-        fragments.append(curr.get_rhombus())
-        to_augment.append(curr.get_rhombus())
+        rhombus = curr.get_rhombus()
+        to_augment.append(rhombus)
+        __save_tile(rhombus)
 
         if curr.dims[0] * 4 <= max_size:
             for i in range(0, 2):
                 for j in range(0, 2):
-                    fragments.append(curr.assemble_quadrant_unfold(i, j))
-                    to_augment.append(curr.assemble_quadrant_unfold(i, j))
+                    unfolded = curr.assemble_quadrant_unfold(i, j)
+                    to_augment.append(unfolded)
+                    __save_tile(unfolded)
 
-            fragments.append(curr.assemble_quadrant_windmill())
-            to_augment.append(curr.assemble_quadrant_windmill())
 
-    return fragments
+            windmill = curr.assemble_quadrant_windmill()
+            to_augment.append(windmill)
+            __save_tile(windmill)
 
 
 def enrich_colour(tile):
@@ -86,23 +101,3 @@ def enrich_contrast(tile):
     img = tile.img
     channels = list(combinations([0, 1, 2], 1)) + list(combinations([0, 1, 2], 2))
     return [Tile.Tile(image_utils.increase_contrast(img, _)) for _ in channels]
-
-
-def enrich(tile, min_size, max_size):
-    """
-    Rearranges and recolours starting tile to obtained a list of new tiles.
-
-    :param tile: starting tile
-    :param min_size: minimum size of obtained tile
-    :param max_size: maximum size of obtained tile
-    :return: list of generated tiles
-    """
-    rearranged = deque(rearrange(tile, min_size, max_size))
-    result = deque()
-    while rearranged:
-        curr = rearranged.pop()
-        curr_contrasts = enrich_contrast(curr)
-        for curr_contrasting in curr_contrasts + [curr]:
-            result.extend(enrich_colour(curr_contrasting))
-
-    return list(result)
