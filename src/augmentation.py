403c7694b3475bcfae286b9f0903025af470a534
IrynaSyzova <ira.sizova@gmail.com>
2020-09-19 18:36:11 +0100

    renaming

--- src/augmentation.py
+++ src/augmentation.py
@@ -1,146 +1,98 @@
 import uuid
+from skimage.metrics import structural_similarity
 
-from collections import deque
-from itertools import product
-
-from src import logging_utils, s3_utils
+from src import logging_utils, s3_utils, image_utils
 from src.Tile import Tile
+
+MIN_SIZE = 64
+MAX_SIZE = 2500
+MAX_SIMILARITY = 0.7
 
 logger = logging_utils.get_logger(__name__)
 
 
-def enrich(tile, key, scale_min=0.25, scale_max=4):
-    """
-    Creates a list of new tiles obtained by cutting and gluing current tile;
-    saves resulting images to s3
+def enrich(tile, key, max_fragmentation_depth=2, max_augmentation_depth=2, max_overall_depth=4):
+    temp_key = '{}/temp'.format(key)
+    __enrich(__save_tile(tile, temp_key), key, temp_key, max_fragmentation_depth, max_augmentation_depth,
+             max_overall_depth)
+    s3_utils.delete_from_s3(temp_key)
 
-    :param tile: starting tile
-    :param key: path in s3 to save generated images
-    :param scale_min: how small minimum size of obtained tile can be relative to starting tile by dimension
-    :param scale_max: how big maximum size of obtained tile can be relative to starting tile by dimension
-    :return: None
-    """
 
-    min_size = int(tile.dims[0] * scale_min)
-    max_size = int(tile.dims[0] * scale_max)
-
-    logger.info('Enriching tile of {x}x{x} dims; generated tiles will be from {y}x{y} to {z}x{z}'.format(
-        x=tile.dims[0],
-        y=min_size,
-        z=max_size
+def __enrich(tile_path, key, temp_key, max_fragmentation_depth=2, max_augmentation_depth=2, max_overall_depth=4):
+    logger.info('Fragmentation depth: {}, augmentation depth: {}, overall depth: {}'.format(
+        max_fragmentation_depth, max_augmentation_depth, max_overall_depth
     ))
 
-    def __save_tile(tile_save, key):
-        """
-        Recolours and saves the tile at path key with randomly generated name
-        :param tile_save: tile to save
-        :param key: path to save image
-        :return: path to saved file
-        """
-        logger.debug('Saving img of {} dims.'.format(tile_save.dims))
-        img_path = '{}/{}.jpg'.format(key, str(uuid.uuid4()))
-        s3_utils.write_image_to_s3(tile_save.img, img_path)
-        logger.debug('Finished saving.')
-        return img_path
+    imgs_in_key = len(s3_utils.get_image_list_from_s3(key))
+    imgs_in_temp_key = len(s3_utils.get_image_list_from_s3(temp_key))
 
-    def __read_tile(key):
-        """
-        Reads image from s3 and converts it to Tile type
-        :param key: path to read image from
-        :return: Tile
-        """
-        return Tile(s3_utils.read_image_from_s3(key))
+    logger.info('{} images created'.format(
+        imgs_in_key - imgs_in_temp_key
+    ))
 
-    to_fragment, to_fragment_key = deque(), '{}/to_fragment'.format(key)
-    to_augment, to_augment_key = deque(), '{}/to_augment'.format(key)
+    tile = __read_tile(tile_path)
 
-    to_fragment.append(__save_tile(tile, to_fragment_key))
+    if tile.dims[0] <= MIN_SIZE:
+        max_fragmentation_depth = 0
 
-    while to_fragment:
-        curr = __read_tile(to_fragment.popleft())
+    if max_fragmentation_depth > 0 and max_overall_depth > 0:
 
-        _ = __save_tile(curr, key)
-
-        if curr.dims[0] / 2 * 2**2 >= min_size:
-            rhombus = curr.get_rhombus()
-            if rhombus.dims[0] >= min_size:
-                to_fragment.append(__save_tile(rhombus, to_fragment_key))
-                to_augment.append(__save_tile(rhombus, to_augment_key))
-
-            _ = __save_tile(rhombus, key)
-
-        if curr.dims[0] / 2 >= min_size:
-            quadrants = [
-                curr.get_quadrant(0, 0),
-                curr.get_quadrant(0, 1).rotate(clockwise=False),
-                curr.get_quadrant(1, 0).rotate(clockwise=True),
-                curr.get_quadrant(1, 1).rotate().rotate(),
+        fragments = [
+            __save_tile(tile.get_rhombus(), temp_key),
+            __save_tile(tile.get_quadrant(0, 0), temp_key),
+        ]
+        if __similarity(tile.get_quadrant(0,0), tile.get_quadrant(1, 0).rotate(clockwise=True)) <= MAX_SIMILARITY:
+            fragments += [
+                __save_tile(tile.get_quadrant(0, 1).rotate(clockwise=False), temp_key),
+                __save_tile(tile.get_quadrant(1, 0).rotate(clockwise=True), temp_key),
+                __save_tile(tile.get_quadrant(1, 1).rotate().rotate(), temp_key)
             ]
-            for quadrant in quadrants:
-                to_fragment.append(__save_tile(quadrant, to_fragment_key))
-                to_augment.append(__save_tile(quadrant, to_augment_key))
-                _ = __save_tile(quadrant, key)
+        for fragment in fragments:
+            __enrich(fragment, key, temp_key, max_fragmentation_depth - 1, max_augmentation_depth,
+                     max_overall_depth - 1)
 
-        if curr.dims[0] <= max_size:
-            to_augment.append(__save_tile(curr, to_augment_key))
+    if max_augmentation_depth > 0 and max_overall_depth > 0:
+        __enrich(__save_tile(tile.add_border_reflect(border_thickness=0.5), temp_key), key, temp_key,
+                 max_fragmentation_depth, max_augmentation_depth - 1, max_overall_depth - 1)
 
-        logger.info('{} files in fragmentation queue, {} files in augmentation queue'.format(
-            len(to_fragment), len(to_augment)
-        ))
+        __enrich(__save_tile(tile.assemble_quadrant_unfold(0, 0), temp_key), key, temp_key, max_fragmentation_depth + 1,
+                 max_augmentation_depth - 1,
+                 max_overall_depth - 1)
 
-    while to_augment:
-        curr = __read_tile(to_augment.pop())
+        if max_fragmentation_depth > 0 and max_overall_depth > 0:
+            __enrich(__save_tile(tile.assemble_quadrant_unfold(0, 0).remove_center(), temp_key), key, temp_key,
+                     max_fragmentation_depth - 1, max_augmentation_depth - 1, max_overall_depth - 1)
 
-        reflect_border_thickness = 0.5
-        if curr.dims[0] * (1+2*reflect_border_thickness) <= max_size:
-            border_reflect = curr.add_border_reflect(border_thickness=reflect_border_thickness)
-            border_reflect_rhombus = border_reflect.get_rhombus()
+    _ = __save_tile(tile, key)
+    _ = __save_tile(tile.add_border_reflect(border_thickness=0.25), key)
+    _ = __save_tile(tile.add_border_reflect(border_thickness=0.33), key)
 
-            _ = __save_tile(border_reflect, key)
-            _ = __save_tile(border_reflect_rhombus, key)
-
-            to_augment.append(__save_tile(border_reflect, to_augment_key))
-
-        reflect_border_thickness = 0.33
-        if curr.dims[0] * (1 + 2 * reflect_border_thickness) <= max_size:
-            border_reflect = curr.add_border_reflect(border_thickness=reflect_border_thickness)
-
-            _ = __save_tile(border_reflect, key)
-
-        reflect_border_thickness = 0.25
-        if curr.dims[0] * (1 + 2 * reflect_border_thickness) <= max_size:
-            border_reflect = curr.add_border_reflect(border_thickness=reflect_border_thickness)
-            _ = __save_tile(border_reflect, key)
-
-            if border_reflect.dims[0] * 2 <= max_size:
-                border_reflect_unfolded = border_reflect.assemble_quadrant_unfold(0, 0)
-                _ = __save_tile(border_reflect_unfolded, key)
-
-        if curr.dims[0] * 2 <= max_size:
-            unfolded = curr.assemble_quadrant_unfold(0, 0)
-
-            _ = __save_tile(unfolded, key)
-            _ = __save_tile(unfolded.get_rhombus(), key)
-            _ = __save_tile(unfolded.remove_center(), key)
-
-            to_augment.append(__save_tile(unfolded, to_augment_key))
-
-        logger.info('{} files in fragmentation queue, {} files in augmentation queue'.format(
-            len(to_fragment), len(to_augment)
-        ))
-
-    s3_utils.delete_from_s3(to_augment_key)
-    s3_utils.delete_from_s3(to_fragment_key)
+    return
 
 
-def enrich_colour(tile):
+def __save_tile(tile_save, key):
     """
-    Returns a list of tiles obtained from starting tile by recolouring it.
-
-    :param tile: starting tile
-    :return: list of recoloured tiles
+    Recolours and saves the tile at path key with randomly generated name
+    :param tile_save: tile to save
+    :param key: path to save image
+    :return: path to saved file
     """
-    channels = product([0, 1, 2], repeat=3)
-    return [tile.recolour(_)
-            for _ in channels]
+    if tile_save.dims[0] >= MAX_SIZE:
+        tile_save = Tile(image_utils.resize(tile_save.img, (MAX_SIZE, MAX_SIZE)))
+    logger.debug('Saving img of {} dims.'.format(tile_save.dims))
+    img_path = '{}/{}.jpg'.format(key, str(uuid.uuid4()))
+    s3_utils.write_image_to_s3(tile_save.img, img_path)
+    logger.debug('Finished saving.')
+    return img_path
 
+
+def __read_tile(key):
+    """
+    Reads image from s3 and converts it to Tile type
+    :param key: path to read image from
+    :return: Tile
+    """
+    return Tile(s3_utils.read_image_from_s3(key))
+
+def __similarity(tile1, tile2):
+    return structural_similarity(tile1.img, tile2.img, multichannel=True)
