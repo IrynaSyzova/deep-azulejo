e2c42057c85f825f1fc62779198babf6f6f03a19
IrynaSyzova <ira.sizova@gmail.com>
2020-09-19 15:13:45 +0100

    update

--- src/augmentation.py
+++ src/augmentation.py
@@ -70,13 +70,6 @@ def enrich(tile, key, scale_min=0.25, scale_max=4):
 
             _ = __save_tile(rhombus, key)
 
-        if curr.dims[0] / 3 * 2 >= min_size:
-            no_center = curr.remove_center()
-            if no_center.dims[0] >= min_size:
-                to_fragment.append(__save_tile(no_center, to_fragment_key))
-                to_augment.append(__save_tile(no_center, to_augment_key))
-            _ = __save_tile(no_center, key)
-
         if curr.dims[0] / 2 >= min_size:
             quadrants = [
                 curr.get_quadrant(0, 0),
