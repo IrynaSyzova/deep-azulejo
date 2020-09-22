5f148688d0a290688cfe906c959154a7de4909b5
IrynaSyzova <ira.sizova@gmail.com>
2020-09-22 21:25:25 +0100

    adding some gan redefinitions

---
+++ src/modelling/GANItemListS3.py
@@ -0,0 +1,27 @@
+import PIL
+import warnings
+import numpy as np
+
+from fastai.vision.gan import GANItemList
+from fastai.vision.image import Image, pil2tensor
+
+from src import s3_utils
+
+
+class GANItemListS3(GANItemList):
+    def open(self, s3_path):
+        return open_image(s3_path, convert_mode=self.convert_mode, after_open=self.after_open)
+
+    @classmethod
+    def from_s3(cls, s3_path):
+        return cls(s3_utils.get_image_list_from_s3(s3_path))
+
+
+def open_image(s3_path, convert_mode='RGB', cls=Image, after_open=None):
+    "Return `Image` object created from image in file `fn`."
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", UserWarning) # EXIF warning from TiffPlugin
+        x = PIL.Image.open(s3_path).convert(convert_mode)
+    if after_open: x = after_open(x)
+    x = pil2tensor(x,np.float32)
+    return cls(x)
