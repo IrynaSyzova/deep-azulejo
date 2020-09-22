521af74a20798abf496f6e6f6c4412c98c26c98e
IrynaSyzova <ira.sizova@gmail.com>
2020-09-22 21:49:33 +0100

    update

--- src/modelling/GANItemListS3.py
+++ src/modelling/GANItemListS3.py
@@ -13,18 +13,18 @@ class GANItemListS3(GANItemList):
         super().__init__(*args, **kwargs)
 
     def open(self, s3_path):
-        return __open_image(s3_path, convert_mode=self.convert_mode, after_open=self.after_open)
+        return open_image(s3_path, convert_mode=self.convert_mode, after_open=self.after_open)
 
     @classmethod
     def from_s3(cls, s3_path, **kwargs):
         return cls(s3_utils.get_image_list_from_s3(s3_path), path='', **kwargs)
 
 
-def __open_image(s3_path, convert_mode='RGB', cls=Image, after_open=None):
-    "Return `Image` object created from image in file `fn`."
+def open_image(s3_path, div=True, convert_mode='RGB', cls=Image, after_open=None):
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", UserWarning) # EXIF warning from TiffPlugin
         x = PIL.Image.open(s3_path).convert(convert_mode)
     if after_open: x = after_open(x)
     x = pil2tensor(x,np.float32)
+    if div: x.div_(255)
     return cls(x)
